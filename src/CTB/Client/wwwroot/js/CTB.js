a011f785289300e13fd75a01ad0ede146ab6a7db
Janne Mattila <janne.mattila@hotmail.fi>
2020-12-05 07:54:14 +0200

    Add user id generation

--- src/CTB/Client/wwwroot/js/CTB.js
+++ src/CTB/Client/wwwroot/js/CTB.js
@@ -9,6 +9,34 @@ window.addEventListener('resize', function () {
         CTB.draw(undefined);
     }
 });
+CTB.getUserId = function () {
+    var id = "";
+    var CatchTheBananaUserId = "CatchTheBananaUserId";
+    var searchText = CatchTheBananaUserId + "=";
+    var startIndex = document.cookie.indexOf(searchText);
+    if (startIndex === -1) {
+        try {
+            var random = window.crypto.getRandomValues(new Uint32Array(4));
+            id = random[0].toString(16) + "-" + random[1].toString(16) + "-" + random[2].toString(16) + "-" + random[3].toString(16);
+        }
+        catch (e) {
+            console.log("Secure random number generation is not supported.");
+            id = Math.floor(Math.random() * 10000000000).toString();
+        }
+        document.cookie = CatchTheBananaUserId + "=" + id + "; max-age=" + 3600 * 12 + "; secure; samesite=strict";
+    }
+    else {
+        startIndex = startIndex + searchText.length;
+        var endIndex = document.cookie.indexOf(";", startIndex);
+        if (endIndex === -1) {
+            id = document.cookie.substr(startIndex);
+        }
+        else {
+            id = document.cookie.substring(startIndex, endIndex);
+        }
+    }
+    return id;
+};
 CTB.initialize = function (canvasElement) {
     console.log("=> initialize");
     _canvasElement = canvasElement;
--- src/CTB/Client/wwwroot/js/CTB.ts
+++ src/CTB/Client/wwwroot/js/CTB.ts
@@ -12,6 +12,35 @@ window.addEventListener('resize', () => {
     }
 });
 
+CTB.getUserId =  () => {
+    let id = "";
+    const CatchTheBananaUserId = "CatchTheBananaUserId";
+    const searchText = `${CatchTheBananaUserId}=`;
+    let startIndex = document.cookie.indexOf(searchText);
+    if (startIndex === -1) {
+        try {
+            const random = window.crypto.getRandomValues(new Uint32Array(4));
+            id = random[0].toString(16) + "-" + random[1].toString(16) + "-" + random[2].toString(16) + "-" + random[3].toString(16);
+        } catch (e) {
+            console.log("Secure random number generation is not supported.");
+            id = Math.floor(Math.random() * 10000000000).toString();
+        }
+
+        document.cookie = `${CatchTheBananaUserId}=${id}; max-age=${3600 * 12}; secure; samesite=strict`;
+    }
+    else {
+        startIndex = startIndex + searchText.length;
+        const endIndex = document.cookie.indexOf(";", startIndex);
+        if (endIndex === -1) {
+            id = document.cookie.substr(startIndex);
+        }
+        else {
+            id = document.cookie.substring(startIndex, endIndex);
+        }
+    }
+    return id;
+}
+
 CTB.initialize = (canvasElement: HTMLCanvasElement) => {
     console.log("=> initialize");
     _canvasElement = canvasElement;
