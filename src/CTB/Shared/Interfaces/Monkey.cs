3be837f0d5eb22f95d51ddd8f08059ac10aa18ab
Janne Mattila <janne.mattila@hotmail.fi>
2020-12-16 19:30:12 +0200

    Add interface classes

---
+++ src/CTB/Shared/Interfaces/Monkey.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CTB.Shared.Interfaces
+{
+    public class Monkey
+    {
+        [JsonPropertyName("id")]
+        public string ID { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("score")]
+        public int Score { get; set; }
+
+        [JsonPropertyName("position")]
+        public Position Position { get; set; }
+
+        public Monkey()
+        {
+            Position = new Position();
+        }
+    }
+}
---
+++ src/CTB/Shared/Interfaces/Position.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CTB.Shared.Interfaces
+{
+    public class Position
+    {
+        [JsonPropertyName("x")]
+        public int X { get; set; }
+
+        [JsonPropertyName("y")]
+        public int Y { get; set; }
+    }
+}
