1f1d73f824efd33b41b5c6ae26cb185de1e956f9
Janne Mattila <janne.mattila@hotmail.fi>
2021-01-05 21:03:05 +0200

    Fix typo on field name

--- src/CTB/Shared/WorldConstants.cs
+++ src/CTB/Shared/WorldConstants.cs
@@ -7,7 +7,7 @@ namespace CTB.Shared
         public const int BorderRadius = 10;
 
         public static readonly Size Screen = new(640, 480);
-        public static readonly Size Banan = new(14, 12);
+        public static readonly Size Banana = new(14, 12);
         public static readonly Size Monkey = new(16, 16);
         public static readonly Size Shark = new(70, 13);
     }
